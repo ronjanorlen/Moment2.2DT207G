63a7ed0bdd3aa365d813ddcf4047a823ff485778
Ronja Norlén <rono2300@student.miun.se>
2024-04-09 13:13:48 +0200

    Testar skriva ut jobb från databasen

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* Variabler */
-const url = "https://moment-2-dt207g.onrender.com/"; // API
+const url = "https://moment-2-dt207g.onrender.com/api/workexperience"; // API
 
 /* När sidan laddas, kör init-funktion */
 window.onload = init();
@@ -10,18 +10,39 @@ window.onload = init();
 async function init() {
     try {
         const response = await fetch(url);
-        let data = await response.json();
+        let jobData = await response.json();
 
         // TA BORT SEN
-        console.table(data);
+        console.table(jobData);
 
         // Ta med data till ny funktion
-       // displayJobs(data);
+        displayJobs(jobData);
 
     } catch (e) {
         console.log(e);
         document.getElementById('error').innerHTML = "<p>Något gick fel vid hämtning av jobb</p>";
     }
+}
+
+/* Visa jobb som lagts till */
+function displayJobs(jobData) {
+    const jobEl = document.getElementById('allJobs');
+
+    // Skapa ul-element
+    const ul = document.createElement('ul');
+
+    // Skapa nytt li-element för varje jobb
+    jobData.forEach((job) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = job.companyname;
+
+        // Lägg till li-element till ul
+        ul.appendChild(listItem);
+    
+    });
+
+    // Lägg till i div
+    jobEl.appendChild(ul);
 }
 
 
